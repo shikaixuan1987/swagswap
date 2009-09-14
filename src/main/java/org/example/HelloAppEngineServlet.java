44f6a363a3ad7d66569523a49fe3e0e0cb430e40
sambrodkin <sambrodkin@d75cd664-9d54-11de-b988-bd14ec207769>
2009-09-14 15:27:55 +0000

    add service layer and fix a few small problems

--- src/main/java/org/example/HelloAppEngineServlet.java
+++ src/main/java/org/example/HelloAppEngineServlet.java
@@ -5,11 +5,11 @@ import javax.servlet.http.*;
 
 import org.apache.log4j.Logger;
 
-import com.itcommand.service.SwagItemServiceImpl;
+import com.itcommand.dao.SwagItemDaoImpl;
 
 public class HelloAppEngineServlet extends HttpServlet {
 
-	private static final Logger log = Logger.getLogger(SwagItemServiceImpl.class);
+	private static final Logger log = Logger.getLogger(SwagItemDaoImpl.class);
 	private static final long serialVersionUID = 1L;
 
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)

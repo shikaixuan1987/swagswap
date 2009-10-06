445b95f21ee5f23b6961c77eb6ccd690fbfa268d
sambrodkin <sambrodkin@d75cd664-9d54-11de-b988-bd14ec207769>
2009-10-06 14:19:31 +0000

    Fixes issue 31 put ability to rate in list view

--- src/main/java/com/swagswap/web/springmvc/tags/ShowRatingStarsTag.java
+++ src/main/java/com/swagswap/web/springmvc/tags/ShowRatingStarsTag.java
@@ -5,17 +5,36 @@ import java.io.IOException;
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.tagext.SimpleTagSupport;
 
-import com.google.appengine.api.users.UserService;
-import com.google.appengine.api.users.UserServiceFactory;
+import com.swagswap.domain.SwagItemRating;
+import com.swagswap.domain.SwagSwapUser;
 
 public class ShowRatingStarsTag  extends SimpleTagSupport {
 	public static final String RATED_IMAGE = "starOn.gif";
 	public static final String NOT_RATED_IMAGE = "starOff.gif";
 	// tag attributes
 	private Integer numberOfStarsToShow;
+	//the following are optional (should have userRating or swagSwapUser and swagaItemKey to calculate rating)
 	private Integer userRating; //optional
+	private SwagSwapUser swagSwapUser; //optional
+	private Long swagItemKey; 
 	
+	/**
+	 * Gets user rating and shows appropriate stars
+	 */
 	public void doTag() throws JspException, IOException {
+		Integer userRating = this.userRating;
+		if (userRating==null) { //we'll have to look it up
+			if (swagSwapUser==null) {
+				throw new RuntimeException("tag requires either userRating or swagSwapUser and swagItemKey");
+			}
+			SwagItemRating swagItemRating = swagSwapUser.getSwagItemRating(swagItemKey);
+			if (swagItemRating==null) {
+				userRating=0;
+			}
+			else {
+				userRating=swagItemRating.getUserRating();
+			}
+		}
 		String starImage = (userRating.equals(numberOfStarsToShow)) ? RATED_IMAGE : NOT_RATED_IMAGE;
 		for (int i=0;i<numberOfStarsToShow;i++) {
 			getJspContext().getOut().println("<img src=\"/images/" + starImage + "\" border=\"0\"/>");
@@ -29,5 +48,15 @@ public class ShowRatingStarsTag  extends SimpleTagSupport {
 	public void setUserRating(Integer userRating) {
 		this.userRating = userRating;
 	}
+
+	public void setSwagSwapUser(SwagSwapUser swagSwapUser) {
+		this.swagSwapUser = swagSwapUser;
+	}
+
+	public void setSwagItemKey(Long swagItemKey) {
+		this.swagItemKey = swagItemKey;
+	}
+	
+	
 
 }

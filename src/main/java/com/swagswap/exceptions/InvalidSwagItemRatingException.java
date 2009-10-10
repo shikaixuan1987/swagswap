c39a4a99091a74a93e8c103ff4990e37c5af3026
sambrodkin <sambrodkin@d75cd664-9d54-11de-b988-bd14ec207769>
2009-10-10 14:57:14 +0000

    Updates issue 41 test and exception class

---
+++ src/main/java/com/swagswap/exceptions/InvalidSwagItemRatingException.java
@@ -0,0 +1,17 @@
+package com.swagswap.exceptions;
+
+import com.swagswap.domain.SwagItemRating;
+
+public class InvalidSwagItemRatingException extends RuntimeException {
+	
+	private static final long serialVersionUID = 1L;
+
+	public InvalidSwagItemRatingException(String msg) {
+		super(msg);
+	}
+
+	public InvalidSwagItemRatingException(SwagItemRating swagItemRating) {
+		this("SwagItemRating " + swagItemRating + " cannot be saved with null attribute value");
+	}
+
+}

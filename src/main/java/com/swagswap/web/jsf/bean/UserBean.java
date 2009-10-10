edb7581fee18c1b349970f787d6f651fcdbbfd00
stevenson.scott@gmail.com <stevenson.scott@gmail.com@d75cd664-9d54-11de-b988-bd14ec207769>
2009-10-10 16:25:42 +0000

    Added JSF ratings and Ajax.  And fixed incorrect JSTL namespace

--- src/main/java/com/swagswap/web/jsf/bean/UserBean.java
+++ src/main/java/com/swagswap/web/jsf/bean/UserBean.java
@@ -6,6 +6,8 @@ import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpServletRequest;
 
+import com.google.appengine.api.users.UserService;
+import com.swagswap.domain.SwagItem;
 import com.swagswap.domain.SwagSwapUser;
 import com.swagswap.service.SwagSwapUserService;
 
@@ -29,8 +31,12 @@ public class UserBean {
 		return swagSwapUserService.isUserLoggedIn();
 	}
 
+	public String getUserEmail() {
+		return getGoogleUserService().getCurrentUser().getEmail();
+	}
+	
 	public String getUserName() {
-		return swagSwapUserService.getCurrentUser().getEmail();
+		return getGoogleUserService().getCurrentUser().getNickname();
 	}
 
 	public String getLoginURL() {
@@ -42,13 +48,31 @@ public class UserBean {
 	}
 	
 	public SwagSwapUser getLoggedInUser() {
-		return swagSwapUserService.findByEmail(getUserName());
+		return getSwagSwapUserService().findByEmailOrCreate((getUserEmail()));
 	}
 
 	private String getCurrentUrl() {
 		HttpServletRequest request = (HttpServletRequest) FacesContext
 				.getCurrentInstance().getExternalContext().getRequest();
 		return request.getRequestURI();
+	}
+	
+	public Integer getUserRatingForItem(SwagItem swagItem) {
+		// TODO Maybe service should default user rating to zero if it
+		// doesn't exist
+		Integer userItemRating;
+		if (getLoggedInUser().getSwagItemRating(
+				swagItem.getKey()) == null) {
+			userItemRating = 0;
+		} else {
+			userItemRating = getLoggedInUser()
+					.getSwagItemRating(swagItem.getKey()).getUserRating();
+		}
+		//  Temporary hack
+		if (userItemRating == null) {
+			userItemRating = 0;
+		}
+		return userItemRating;
 	}
 
 }

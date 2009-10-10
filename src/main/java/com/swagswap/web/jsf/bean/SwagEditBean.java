8c136e9e70f3589b4a2ffb0cb19bd2caa4389fcc
stevenson.scott@gmail.com <stevenson.scott@gmail.com@d75cd664-9d54-11de-b988-bd14ec207769>
2009-10-10 09:45:01 +0000

    Added JSF view Swag Detail

--- src/main/java/com/swagswap/web/jsf/bean/SwagEditBean.java
+++ src/main/java/com/swagswap/web/jsf/bean/SwagEditBean.java
@@ -1,60 +1,31 @@
 package com.swagswap.web.jsf.bean;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ManagedProperty;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ViewScoped;
 
 import com.swagswap.domain.SwagItem;
-import com.swagswap.service.ItemService;
 
+/**
+ * @author scott
+ * 
+ *         View scope bean to store SwagItem being edited or inserted.
+ *         Simplifies state saving.
+ * 
+ */
 @ManagedBean(name = "swagEditBean")
-@RequestScoped
-public class SwagEditBean {
+@ViewScoped
+public class SwagEditBean implements Serializable {
 
-	
-	
-	// Inject the swagItemService Spring Bean
-	@ManagedProperty(value = "#{swagItemService}")
-	private ItemService itemService;
-	private SwagItem editSwagItem = new SwagItem();
-	private Long selectedRowId;
-	
+	private static final long serialVersionUID = 1L;
+	private SwagItem editSwagItem;
+
 	public SwagEditBean() {
 		super();
 		initialiseSwagItem();
-	}
-	
-	
-	public void populateSwagItem() {
-		editSwagItem = getItemService().get(getSelectedRowId());
-		
-	}
-
-	public void initialiseSwagItem() {
-		editSwagItem = new SwagItem();
-		List<String> tagList = new ArrayList<String>();
-		for (int i = 0; i < 4; i++) {
-			tagList.add("");
-		}
-		editSwagItem.setTags(tagList);
-	}
-
-	public Long getSelectedRowId() {
-		return selectedRowId;
-	}
-
-	public void setSelectedRowId(Long selectedRowId) {
-		this.selectedRowId = selectedRowId;
-	}
-
-	public String actionSaveItem() {
-		getItemService().save(editSwagItem);
-		initialiseSwagItem();
-
-		return "allSwag?faces-redirect=true";
 	}
 
 	public SwagItem getEditSwagItem() {
@@ -65,12 +36,13 @@ public class SwagEditBean {
 		this.editSwagItem = editSwagItem;
 	}
 
-	public ItemService getItemService() {
-		return itemService;
-	}
-
-	public void setItemService(ItemService itemService) {
-		this.itemService = itemService;
+	private void initialiseSwagItem() {
+		editSwagItem = new SwagItem();
+		List<String> tagList = new ArrayList<String>();
+		for (int i = 0; i < 4; i++) {
+			tagList.add("");
+		}
+		editSwagItem.setTags(tagList);
 	}
 
 }

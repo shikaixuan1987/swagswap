3aece269a2e2ad52bb77562d11313d785231af52
stevenson.scott@gmail.com <stevenson.scott@gmail.com@d75cd664-9d54-11de-b988-bd14ec207769>
2009-10-18 18:05:55 +0000

    Added image resizing and partial thumbnail support

--- src/main/java/com/swagswap/service/ItemServiceImpl.java
+++ src/main/java/com/swagswap/service/ItemServiceImpl.java
@@ -17,6 +17,10 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.google.appengine.api.datastore.Blob;
+import com.google.appengine.api.images.Image;
+import com.google.appengine.api.images.ImagesService;
+import com.google.appengine.api.images.ImagesServiceFactory;
+import com.google.appengine.api.images.Transform;
 import com.google.appengine.api.users.User;
 import com.swagswap.dao.ItemDao;
 import com.swagswap.domain.SwagItem;
@@ -29,26 +33,34 @@ import com.swagswap.exceptions.LoadImageFromURLException;
 
 /**
  * For transactionality and will be used for caching.
+ * 
  * @author sam
- *
+ * 
  */
 public class ItemServiceImpl implements ItemService {
 
 	private static final Logger log = Logger.getLogger(ItemServiceImpl.class);
 
+	private static final int THUMBNAIL_WIDTH = 66;
+	private static final int THUMBNAIL_HEIGHT = 50;
+
+	private static final int IMAGE_WIDTH = 283;
+	private static final int IMAGE_HEIGHT = 212;
+
 	@Autowired
 	private ItemDao itemDao;
 	@Autowired
-	private SwagSwapUserService swagSwapUserService; //for saving users to our app
-	//for checking mime type
+	private SwagSwapUserService swagSwapUserService; // for saving users to our
+	// app
+	// for checking mime type
 	private Magic jmimeMagicParser = new Magic();
 
 	public ItemServiceImpl() {
 	}
 
-	//for unit tests
+	// for unit tests
 	protected ItemServiceImpl(ItemDao itemDao) {
-		this.itemDao=itemDao;
+		this.itemDao = itemDao;
 	}
 
 	/**
@@ -57,98 +69,105 @@ public class ItemServiceImpl implements ItemService {
 	public SwagItem get(Long id) {
 		return get(id, false);
 	}
-	
+
 	/**
 	 * 
 	 * @param id
-	 * @param loadSwagImage whether to load swagImage (it is lazy loaded by JDO)
+	 * @param loadSwagImage
+	 *            whether to load swagImage (it is lazy loaded by JDO)
 	 * @return SwagItem if found
-	 * @throws Exception if item not found
+	 * @throws Exception
+	 *             if item not found
 	 */
 	public SwagItem get(Long id, boolean loadSwagImage) {
 		return itemDao.get(id, loadSwagImage);
 	}
-	
-	//GAE doesn't support case-insensitive queries (yet)
+
+	// GAE doesn't support case-insensitive queries (yet)
 	public List<SwagItem> search(String queryString) {
 		return itemDao.search(queryString);
 	}
-	//  compass wasn't working for me
+
+	// compass wasn't working for me
 	/*
-	public Collection<SwagItem> search(String queryString) {
-		CompassHits hits = swagSwapCompass.getCompass().openSearchSession().find("*"+queryString+"*");
-		Set<SwagItem> swagItems = new HashSet<SwagItem>();
-		for (int i = 0; i < hits.length(); i++) {
-			swagItems.add((SwagItem)hits.data(i));
-			
-		}
-		return swagItems;
-	}
-	*/
+	 * public Collection<SwagItem> search(String queryString) { CompassHits hits
+	 * =
+	 * swagSwapCompass.getCompass().openSearchSession().find("*"+queryString+"*"
+	 * ); Set<SwagItem> swagItems = new HashSet<SwagItem>(); for (int i = 0; i <
+	 * hits.length(); i++) { swagItems.add((SwagItem)hits.data(i));
+	 * 
+	 * } return swagItems; }
+	 */
 
 	public List<SwagItem> getAll() {
 		return itemDao.getAll();
 	}
 
 	/**
-	 * saves swag item and image (image is saved in dao because it's a child object)
-	 * (A user is not associated with a swagitem via a JDO relationship because 
-	 * I couldn't get a many-to-one relationship going in JDO), 
-	 * @throws IOException 
-	 * @throws MalformedURLException 
+	 * saves swag item and image (image is saved in dao because it's a child
+	 * object) (A user is not associated with a swagitem via a JDO relationship
+	 * because I couldn't get a many-to-one relationship going in JDO),
+	 * 
+	 * @throws IOException
+	 * @throws MalformedURLException
 	 */
-	
+
 	@Transactional(readOnly = false, propagation = Propagation.SUPPORTS)
-	public void save(SwagItem swagItem) 
-		throws LoadImageFromURLException, ImageTooLargeException, InvalidSwagImageException {
-		if (StringUtils.isEmpty(swagItem.getName())) { //only required field
+	public void save(SwagItem swagItem) throws LoadImageFromURLException,
+			ImageTooLargeException, InvalidSwagImageException {
+		if (StringUtils.isEmpty(swagItem.getName())) { // only required field
 			throw new InvalidSwagItemException("name is required");
 		}
 		if (swagItem.isNew()) {
-			String currentUserEmail = swagSwapUserService.getCurrentUser().getEmail();
+			String currentUserEmail = swagSwapUserService.getCurrentUser()
+					.getEmail();
 			swagItem.setOwnerEmail(currentUserEmail);
-			String currentUserNickName = swagSwapUserService.getCurrentUser().getNickname();
+			String currentUserNickName = swagSwapUserService.getCurrentUser()
+					.getNickname();
 			swagItem.setOwnerNickName(currentUserNickName);
-			
+
 			populateSwagImage(swagItem);
-			
+
 			itemDao.insert(swagItem);
 			/**
-			 * No need to create swagSwapUser here. We only need a user in our DB
-			 * if they rate something (to remember their rating)
-			 * Anyway, this call fails since it tries to update two different entity 
-			 * groups in one transaction
+			 * No need to create swagSwapUser here. We only need a user in our
+			 * DB if they rate something (to remember their rating) Anyway, this
+			 * call fails since it tries to update two different entity groups
+			 * in one transaction
 			 */
-			//swagSwapUserService.findByEmailOrCreate(currentUserEmail);
-		}
-		else { //update
+			// swagSwapUserService.findByEmailOrCreate(currentUserEmail);
+		} else { // update
 			checkPermissions(swagItem.getKey());
 			populateSwagImage(swagItem);
 			itemDao.update(swagItem);
 		}
-		
-		//to test transactions, uncomment the throw exception line below
-		//and try this method with and without the annotation
-		//throw new RuntimeException("see if it rolls back");
+
+		// to test transactions, uncomment the throw exception line below
+		// and try this method with and without the annotation
+		// throw new RuntimeException("see if it rolls back");
 	}
 
 	@Transactional(readOnly = false, propagation = Propagation.SUPPORTS)
-	public synchronized void updateRating(Long swagItemKey, int computedRatingDifference, boolean isNewRating) {
-		//TODO can this line be removed?
+	public synchronized void updateRating(Long swagItemKey,
+			int computedRatingDifference, boolean isNewRating) {
+		// TODO can this line be removed?
 		SwagItem swagItem = get(swagItemKey);
-		itemDao.updateRating(swagItemKey, computedRatingDifference, isNewRating);
+		itemDao
+				.updateRating(swagItemKey, computedRatingDifference,
+						isNewRating);
 	}
 
 	public void delete(Long id) {
 		checkPermissions(id);
 		itemDao.delete(id);
 	}
-	
+
 	public void addComment(SwagItemComment newComment) {
 		if (StringUtils.isEmpty(newComment.getCommentText())) {
 			return;
 		}
-		newComment.setSwagSwapUserNickname(swagSwapUserService.getCurrentUser().getNickname());
+		newComment.setSwagSwapUserNickname(swagSwapUserService.getCurrentUser()
+				.getNickname());
 		itemDao.addComment(newComment);
 	}
 
@@ -160,108 +179,131 @@ public class ItemServiceImpl implements ItemService {
 	public void setSwagSwapUserService(SwagSwapUserService swagSwapUserService) {
 		this.swagSwapUserService = swagSwapUserService;
 	}
-	
-	protected void populateSwagImage(SwagItem swagItem) 
-		throws LoadImageFromURLException, ImageTooLargeException, InvalidSwagImageException {
+
+	protected void populateSwagImage(SwagItem swagItem)
+			throws LoadImageFromURLException, ImageTooLargeException,
+			InvalidSwagImageException {
 		if (!swagItem.hasNewImage()) {
 			return;
 		}
 		byte[] newImageData = null;
 		if (swagItem.hasNewImageBytes()) {
-			//TODO fix this comment
-			// The following line only works for a save, 
-			// not an upate cause there you have to operate on the stored SwagImage
-			//orig.setImage(updatedItem.getImage();
-			 newImageData = swagItem.getImageBytes();
+			// TODO fix this comment
+			// The following line only works for a save,
+			// not an upate cause there you have to operate on the stored
+			// SwagImage
+			// orig.setImage(updatedItem.getImage();
+			newImageData = swagItem.getImageBytes();
 		} else if (swagItem.hasNewImageURL()) {
 			newImageData = getImageDataFromURL(swagItem.getImageURL());
 		}
 		checkImageMimeType(newImageData);
-		swagItem.getImage().setImage(new Blob(newImageData));
+		//  Resize the image before saving
+		swagItem.getImage().setImage(new Blob(getResizedImageBytes(newImageData)));
 	}
 
 	/**
 	 * 
 	 * @return image data from swagItem
 	 */
-	protected byte[] getImageDataFromURL(String imageURL) 
-		throws LoadImageFromURLException, ImageTooLargeException {
-		BufferedInputStream bis=null;
-		ByteArrayOutputStream bos=null;
+	protected byte[] getImageDataFromURL(String imageURL)
+			throws LoadImageFromURLException, ImageTooLargeException {
+		BufferedInputStream bis = null;
+		ByteArrayOutputStream bos = null;
 		try {
-			//fetch URL as InputStream
+			// fetch URL as InputStream
 			URL url = new URL(imageURL);
 			bis = new BufferedInputStream(url.openStream());
-			//write it to a byte[] using a buffer since we don't know the exact image size
-			byte [] buffer = new byte[1024];
+			// write it to a byte[] using a buffer since we don't know the exact
+			// image size
+			byte[] buffer = new byte[1024];
 			bos = new ByteArrayOutputStream();
 			int i = 0;
-		    while (-1 != (i = bis.read(buffer))) {
-		       bos.write(buffer, 0, i);
-		    }
-		    byte[] imageData = bos.toByteArray();
-		    if (imageData.length > 150000) {
-		    	throw new ImageTooLargeException(url.toString(),150000);
-		    }
+			while (-1 != (i = bis.read(buffer))) {
+				bos.write(buffer, 0, i);
+			}
+			byte[] imageData = bos.toByteArray();
+			if (imageData.length > 150000) {
+				throw new ImageTooLargeException(url.toString(), 150000);
+			}
 			return imageData;
-		}
-		catch (IOException e) {
+		} catch (IOException e) {
 			throw new LoadImageFromURLException(imageURL, e);
-		}
-		finally {
+		} finally {
 			try {
-				if (bis!=null) bis.close();
-				if (bos!=null) bos.close();
+				if (bis != null)
+					bis.close();
+				if (bos != null)
+					bos.close();
 			} catch (IOException e) {
-				//ignore
+				// ignore
 			}
 		}
 	}
-	
+
 	/**
 	 * 
 	 * Make sure they're uploading a picture
+	 * 
 	 * @param imageData
 	 * @throws InvalidSwagImageException
 	 */
-	public void checkImageMimeType(byte[] imageData) throws InvalidSwagImageException {
-			String mimeType;
-			try {
-				MagicMatch match = jmimeMagicParser.getMagicMatch(imageData);
-				mimeType = match.getMimeType();
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				log.error(e);
-				throw new InvalidSwagImageException(e);
-			}
-			if (!(
-					("image/gif".equals(mimeType)) ||
-					("image/png".equals(mimeType)) || 
-					("image/jpeg".equals(mimeType))
-				))
-			{
-				throw new InvalidSwagImageException(mimeType);
-			}
+	public void checkImageMimeType(byte[] imageData)
+			throws InvalidSwagImageException {
+		String mimeType;
+		try {
+			MagicMatch match = jmimeMagicParser.getMagicMatch(imageData);
+			mimeType = match.getMimeType();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			log.error(e);
+			throw new InvalidSwagImageException(e);
+		}
+		if (!(("image/gif".equals(mimeType)) || ("image/png".equals(mimeType)) || ("image/jpeg"
+				.equals(mimeType)))) {
+			throw new InvalidSwagImageException(mimeType);
+		}
 	}
-	
+
 	/**
 	 * @param swagItemIdToCheck
-	 * @throws AccessDeniedException if currentUser is not allowed to access an item
+	 * @throws AccessDeniedException
+	 *             if currentUser is not allowed to access an item
 	 */
-	private void checkPermissions(Long swagItemIdToCheck) throws AccessDeniedException {
+	private void checkPermissions(Long swagItemIdToCheck)
+			throws AccessDeniedException {
 		User user = swagSwapUserService.getCurrentUser();
-		//admins can do everything!
+		// admins can do everything!
 		if (swagSwapUserService.isUserAdmin()) {
 			return;
 		}
-		//get item fresh to prevent client side attacks
-		SwagItem swagItemToCheck = get(swagItemIdToCheck); 
-		// Their email doesn't match the swagItem 
-		// (again, if this happened the webapp messed up or someone's trying to hack us)
+		// get item fresh to prevent client side attacks
+		SwagItem swagItemToCheck = get(swagItemIdToCheck);
+		// Their email doesn't match the swagItem
+		// (again, if this happened the webapp messed up or someone's trying to
+		// hack us)
 		if (!user.getEmail().equals(swagItemToCheck.getOwnerEmail())) {
-			throw new AccessDeniedException(
-					swagItemToCheck,
-					user.getEmail());
+			throw new AccessDeniedException(swagItemToCheck, user.getEmail());
 		}
 	}
+
+	private byte[] getResizedImageBytes(byte[] imageBytes) {
+		return resizeImage(imageBytes, IMAGE_WIDTH, IMAGE_HEIGHT);
+	}
+
+	private byte[] getThumbnailBytes(byte[] imageBytes) {
+		return resizeImage(imageBytes, THUMBNAIL_WIDTH, THUMBNAIL_HEIGHT);
+	}
+
+	private byte[] resizeImage(byte[] imageBytes, int resizedWidth,
+			int resizedHeight) {
+
+		ImagesService imagesService = ImagesServiceFactory.getImagesService();
+		Image oldImage = ImagesServiceFactory.makeImage(imageBytes);
+		Transform resize = ImagesServiceFactory.makeResize(resizedWidth,
+				resizedHeight);
+		Image newImage = imagesService.applyTransform(resize, oldImage);
+		return newImage.getImageData();
+	}
+
 }

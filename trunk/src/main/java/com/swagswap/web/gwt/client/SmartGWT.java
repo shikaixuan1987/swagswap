94c791ae0e56206773aef41930fda578f185a92b
sambrodkin <sambrodkin@d75cd664-9d54-11de-b988-bd14ec207769>
2009-10-24 18:28:14 +0000

    Updates Issue 56 add functionality

--- trunk/src/main/java/com/swagswap/web/gwt/client/SmartGWT.java
+++ trunk/src/main/java/com/swagswap/web/gwt/client/SmartGWT.java
@@ -10,7 +10,6 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.rpc.StatusCodeException;
 import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.VerticalPanel;
 import com.smartgwt.client.types.OperatorId;
 import com.smartgwt.client.widgets.IButton;
 import com.smartgwt.client.widgets.Label;
@@ -20,10 +19,11 @@ import com.smartgwt.client.widgets.form.DynamicForm;
 import com.smartgwt.client.widgets.form.events.ItemChangedEvent;
 import com.smartgwt.client.widgets.form.events.ItemChangedHandler;
 import com.smartgwt.client.widgets.form.fields.CheckboxItem;
+import com.smartgwt.client.widgets.form.fields.LinkItem;
 import com.smartgwt.client.widgets.form.fields.SelectItem;
-import com.smartgwt.client.widgets.form.fields.SliderItem;
 import com.smartgwt.client.widgets.form.fields.TextItem;
 import com.smartgwt.client.widgets.layout.HLayout;
+import com.smartgwt.client.widgets.layout.HStack;
 import com.smartgwt.client.widgets.layout.VStack;
 import com.smartgwt.client.widgets.tile.TileGrid;
 import com.smartgwt.client.widgets.tile.events.RecordClickEvent;
@@ -34,27 +34,18 @@ import com.smartgwt.client.widgets.viewer.DetailViewerRecord;
 import com.swagswap.web.gwt.client.domain.LoginInfo;
 
 public class SmartGWT implements EntryPoint {
-	private LoginInfo loginInfo = null;
-	private VerticalPanel loginPanel = new VerticalPanel();
-	private VerticalPanel logoutPanel = new VerticalPanel();
-	private Label loginLabel = new Label(
-			"Please sign in to your Google Account to edit or rate items");
-	private Anchor signInLink = new Anchor("Sign In");
-	private Anchor signOutLink = new Anchor("Sign Out");
-
+	private LoginInfo loginInfo; //null if they're not logged in
 
 	private final SmartGWTItemServiceWrapperAsync service = GWT
 			.create(SmartGWTItemServiceWrapper.class);
 
 	final TileGrid tileGrid = new TileGrid();
 
+	private VStack boundFormVStack;
+
+	private DynamicForm boundSwagForm;
+
 	public void onModuleLoad() {
-
-		final VStack vStack = new VStack(20);
-		// get better exception handling
-		setUncaughtExceptionHandler();
-
-		   // Check login status using login service.
 		LoginServiceAsync loginService = GWT.create(LoginService.class);
 		loginService.login(GWT.getHostPageBaseURL() + "SwagSwapGWT.html",
 				new AsyncCallback<LoginInfo>() {
@@ -64,24 +55,126 @@ public class SmartGWT implements EntryPoint {
 
 					public void onSuccess(LoginInfo result) {
 						loginInfo = result;
-						if (loginInfo.isLoggedIn()) {
-							signOutLink.setHref(loginInfo.getLogoutUrl());
-							logoutPanel.add(signOutLink);
-							vStack.addMember(logoutPanel);
-						} else {
-							signInLink.setHref(loginInfo.getLoginUrl());
-							loginPanel.add(loginLabel);
-							loginPanel.add(signInLink);
-							vStack.addMember(loginPanel);
-						}
+						buildGUI();
 					}
 				});
-	  
+	}
+	
+	public void buildGUI() {
+		final VStack vStack = new VStack(20);
+		vStack.setShowEdges(true);
+		// get better exception handling
+		setUncaughtExceptionHandler();
 
-		
 		vStack.setWidth100();
+		addLoginLogoutPanel(vStack);
+		HStack searchSortHStack= new HStack();
+		final DynamicForm filterForm = addSearchPanel(searchSortHStack);
+		final DynamicForm sortForm = addSortPanel(searchSortHStack);
+		vStack.addMember(searchSortHStack);
+//		HLayout hLayout = addFilterButton(vStack, filterForm, sortForm);
+		addItemsPanel(vStack);
+		addEditForm(vStack);
 
+		RootPanel.get("gwt-tilegrid").add(vStack);
+		vStack.draw();
+	}
+
+	private HLayout addFilterButton(final VStack vStack,
+			final DynamicForm filterForm, final DynamicForm sortForm) {
+		HLayout hLayout = new HLayout(10);
+		hLayout.setHeight(22);
+
+		IButton filterButton = new IButton("Filter");
+		filterButton.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				tileGrid.fetchData(filterForm.getValuesAsCriteria());
+			}
+		});
+		filterButton.setAutoFit(true);
+
+		IButton clearButton = new IButton("Clear");
+		clearButton.setAutoFit(true);
+		clearButton.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				tileGrid.fetchData();
+				filterForm.clearValues();
+				sortForm.clearValues();
+			}
+		});
+
+		hLayout.addMember(filterButton);
+		hLayout.addMember(clearButton);
+		vStack.addMember(hLayout);
+		return hLayout;
+	}
+
+	private DynamicForm addSortPanel(final HStack hStack) {
+		final DynamicForm sortForm = new DynamicForm();
+		sortForm.setIsGroup(true);
+		sortForm.setGroupTitle("Sort");
+		sortForm.setAutoFocus(false);
+		sortForm.setNumCols(6);
+
+		SelectItem sortItem = new SelectItem();
+		sortItem.setName("sortBy");
+		sortItem.setTitle("Sort By");
+
+		LinkedHashMap valueMap = new LinkedHashMap();
+		valueMap.put("id", "Id");
+		valueMap.put("name", "Name");
+		valueMap.put("date", "Date");
+
+		sortItem.setValueMap(valueMap);
+
+		final CheckboxItem ascendingItem = new CheckboxItem("chkSortDir");
+		ascendingItem.setTitle("Ascending");
+
+		sortForm.setFields(sortItem, ascendingItem);
+
+		sortForm.addItemChangedHandler(new ItemChangedHandler() {
+			public void onItemChanged(ItemChangedEvent event) {
+				String sortVal = sortForm.getValueAsString("sortBy");
+				Boolean sortDir = (Boolean) ascendingItem.getValue();
+				if (sortDir == null)
+					sortDir = false;
+				if (sortVal != null) {
+					tileGrid.sortByProperty(sortVal, sortDir);
+				}
+			}
+		});
+		hStack.addMember(sortForm);
+		return sortForm;
+	}
+
+	private DynamicForm addSearchPanel(final HStack hStack) {
+		final DynamicForm filterForm = new DynamicForm();
+		filterForm.setIsGroup(true);
+		filterForm.setGroupTitle("Search");
+		filterForm.setNumCols(6);
+		filterForm.setDataSource(SmartGWTRPCDataSource.getInstance());
+		filterForm.setAutoFocus(false);
+
+		TextItem nameItem = new TextItem("name");
+		SelectItem companyItem = new SelectItem("company");
+		companyItem.setOperator(OperatorId.CONTAINS); // fuzzy search!
+		companyItem.setAllowEmptyValue(true);
+
+		filterForm.setFields(nameItem, companyItem);
+
+		filterForm.addItemChangedHandler(new ItemChangedHandler() {
+			public void onItemChanged(ItemChangedEvent event) {
+				tileGrid.fetchData(filterForm.getValuesAsCriteria());
+			}
+		});
+
+		hStack.addMember(filterForm);
+		return filterForm;
+	}
+
+	private void addItemsPanel(final VStack vStack) {
 		tileGrid.setTileWidth(150);
+		tileGrid.setTileValueAlign("left");
 		tileGrid.setTileHeight(205);
 		tileGrid.setHeight(400);
 		tileGrid.setShowAllRecords(true);
@@ -131,102 +224,48 @@ public class SmartGWT implements EntryPoint {
 				ownerNickName, averageRating, numberOfRatings, lastUpdated);
 
 		vStack.addMember(tileGrid);
-
-		final DynamicForm filterForm = new DynamicForm();
-		filterForm.setIsGroup(true);
-		filterForm.setGroupTitle("Search");
-		filterForm.setNumCols(6);
-		filterForm.setDataSource(SmartGWTRPCDataSource.getInstance());
-		filterForm.setAutoFocus(false);
-
-		TextItem commonNameItem = new TextItem("name");
-		SliderItem lifeSpanItem = new SliderItem("id");
-		lifeSpanItem.setTitle("Max Life Span");
-		lifeSpanItem.setMinValue(1);
-		lifeSpanItem.setMaxValue(60);
-		lifeSpanItem.setDefaultValue(60);
-		lifeSpanItem.setHeight(50);
-		lifeSpanItem.setOperator(OperatorId.LESS_THAN);
-
-		SelectItem statusItem = new SelectItem("name");
-		statusItem.setOperator(OperatorId.CONTAINS); // fuzzy search!
-		statusItem.setAllowEmptyValue(true);
-
-		filterForm.setFields(commonNameItem, lifeSpanItem, statusItem);
-
-		filterForm.addItemChangedHandler(new ItemChangedHandler() {
-			public void onItemChanged(ItemChangedEvent event) {
-				tileGrid.fetchData(filterForm.getValuesAsCriteria());
-			}
-		});
-
-		vStack.addMember(filterForm);
-
-		final DynamicForm sortForm = new DynamicForm();
-		sortForm.setIsGroup(true);
-		sortForm.setGroupTitle("Sort");
-		sortForm.setAutoFocus(false);
-		sortForm.setNumCols(6);
-
-		SelectItem sortItem = new SelectItem();
-		sortItem.setName("sortBy");
-		sortItem.setTitle("Sort By");
-
-		LinkedHashMap valueMap = new LinkedHashMap();
-		valueMap.put("id", "Id");
-		valueMap.put("name", "Name");
-		valueMap.put("date", "Date");
-
-		sortItem.setValueMap(valueMap);
-
-		final CheckboxItem ascendingItem = new CheckboxItem("chkSortDir");
-		ascendingItem.setTitle("Ascending");
-
-		sortForm.setFields(sortItem, ascendingItem);
-
-		sortForm.addItemChangedHandler(new ItemChangedHandler() {
-			public void onItemChanged(ItemChangedEvent event) {
-				String sortVal = sortForm.getValueAsString("sortBy");
-				Boolean sortDir = (Boolean) ascendingItem.getValue();
-				if (sortDir == null)
-					sortDir = false;
-				if (sortVal != null) {
-					tileGrid.sortByProperty(sortVal, sortDir);
-				}
-			}
-		});
-		vStack.addMember(sortForm);
-
-		HLayout hLayout = new HLayout(10);
-		hLayout.setHeight(22);
-
-		IButton filterButton = new IButton("Filter");
-		filterButton.addClickHandler(new ClickHandler() {
+	}
+	
+	private void addLoginLogoutPanel(final VStack vStack) {
+		HStack loginPanel = new HStack();
+		loginPanel.setWidth100();
+		loginPanel.setHeight(10);
+		HStack logoutPanel = new HStack();
+		logoutPanel.setWidth100();
+		logoutPanel.setHeight(10);
+		Anchor signInLink = new Anchor("Sign In");
+		Anchor signOutLink = new Anchor("Sign Out");
+		Label addLink = new Label("Add Swag");
+		addLink.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
-				tileGrid.fetchData(filterForm.getValuesAsCriteria());
-			}
-		});
-		filterButton.setAutoFit(true);
-
-		IButton clearButton = new IButton("Clear");
-		clearButton.setAutoFit(true);
-		clearButton.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				tileGrid.fetchData();
-				filterForm.clearValues();
-				sortForm.clearValues();
+				boundFormVStack.show();
+				boundSwagForm.editNewRecord();
 			}
 		});
 
-		hLayout.addMember(filterButton);
-		hLayout.addMember(clearButton);
-		vStack.addMember(hLayout);
-
-		// form
-		final DynamicForm boundForm = new DynamicForm();
-		boundForm.setNumCols(6);
-		boundForm.setDataSource(SmartGWTRPCDataSource.getInstance());
-		boundForm.setAutoFocus(false);
+		if (loginInfo.isLoggedIn()) {
+			signOutLink.setHref(loginInfo.getLogoutUrl());
+			logoutPanel.addMember(signOutLink);
+			Label welcomeLabel = new Label("Welcome: "
+					+ loginInfo.getNickname());
+			welcomeLabel.setWrap(false);
+			logoutPanel.addMember(welcomeLabel);
+			logoutPanel.addMember(addLink);
+			vStack.addMember(logoutPanel);
+		} else {
+			signInLink.setHref(loginInfo.getLoginUrl());
+			loginPanel.addMember(signInLink);
+			loginPanel.addMember(addLink);
+			vStack.addMember(loginPanel);
+		}
+	}
+	
+	private void addEditForm(VStack vStack) {
+		boundFormVStack = new VStack();
+		boundSwagForm = new DynamicForm();
+		boundSwagForm.setNumCols(6);
+		boundSwagForm.setDataSource(SmartGWTRPCDataSource.getInstance());
+		boundSwagForm.setAutoFocus(false);
 
 		TextItem nameItem = new TextItem("name");
 		TextItem companyItem = new TextItem("company");
@@ -236,36 +275,47 @@ public class SmartGWT implements EntryPoint {
 		TextItem tag3Item = new TextItem("tag3");
 		TextItem tag4Item = new TextItem("tag4");
 
-		boundForm.setFields(nameItem, companyItem, descriptionItem, tag1Item,
+		boundSwagForm.setFields(nameItem, companyItem, descriptionItem, tag1Item,
 				tag2Item, tag3Item, tag4Item);
-		vStack.addMember(boundForm);
+		boundFormVStack.addMember(boundSwagForm);
 
 		tileGrid.addRecordClickHandler(new RecordClickHandler() {
 			public void onRecordClick(RecordClickEvent event) {
-				boundForm.editRecord(event.getRecord());
+				boundFormVStack.show();
+				boundSwagForm.editRecord(event.getRecord());
 			}
 		});
 		//
 		// HLayout hLayout = new HLayout(10);
 		// hLayout.setHeight(22);
 
-		IButton button = new IButton("Save");
-		button.setAutoFit(true);
-		button.addClickHandler(new ClickHandler() {
+		IButton saveButton = new IButton("Save");
+		saveButton.setAutoFit(true);
+		saveButton.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
-				boundForm.saveData();
-				if (boundForm.hasErrors()) {
-					Window.alert("" + boundForm.getErrors());
+				boundSwagForm.saveData();
+				if (boundSwagForm.hasErrors()) {
+					Window.alert("" + boundSwagForm.getErrors());
 				} else {
-					boundForm.clearValues();
+					boundSwagForm.clearValues();
+					boundFormVStack.hide();
 				}
 			}
 		});
-		hLayout.addMember(button);
-		vStack.addMember(hLayout);
-
-		RootPanel.get("gwt-tilegrid").add(vStack);
-		vStack.draw();
+		IButton cancelButton = new IButton("Cancel");
+		cancelButton.setAutoFit(true);
+		cancelButton.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				boundSwagForm.clearValues();
+				boundFormVStack.hide();
+			}
+		});
+		HLayout hLayout = new HLayout();
+		hLayout.addMember(saveButton);
+		hLayout.addMember(cancelButton);
+		boundFormVStack.addMember(hLayout);
+		boundFormVStack.hide();
+		vStack.addMember(boundFormVStack);
 	}
 
 	private void setUncaughtExceptionHandler() {

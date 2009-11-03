31bed5710d6f90a039b3de6f4d511c715c38f819
sambrodkin <sambrodkin@d75cd664-9d54-11de-b988-bd14ec207769>
2009-11-03 12:37:28 +0000

    make serializable for GWT

--- src/main/java/com/swagswap/exceptions/ImageTooLargeException.java
+++ src/main/java/com/swagswap/exceptions/ImageTooLargeException.java
@@ -5,6 +5,11 @@ public class ImageTooLargeException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 	private String url;
 
+	//Needed for GWT compilation
+	public ImageTooLargeException() {
+		super();
+	}
+	
 	public ImageTooLargeException(String url, int maxSize) {
 		super("Image at url " + url+ " is too large (max size is " + maxSize + ")");
 	}
--- src/main/java/com/swagswap/exceptions/LoadImageFromURLException.java
+++ src/main/java/com/swagswap/exceptions/LoadImageFromURLException.java
@@ -5,6 +5,11 @@ public class LoadImageFromURLException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 	private String url;
 
+	//Needed for GWT compilation
+	public LoadImageFromURLException() {
+		super();
+	}
+	
 	public LoadImageFromURLException(String url, Throwable cause) {
 		super("Cannot load image from " + url, cause);
 		this.url=url;

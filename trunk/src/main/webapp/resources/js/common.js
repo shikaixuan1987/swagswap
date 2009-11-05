7b4c585fec60ae24c0b48b4a463d3ad46beecd27
stevenson.scott@gmail.com <stevenson.scott@gmail.com@d75cd664-9d54-11de-b988-bd14ec207769>
2009-11-05 00:52:41 +0000

    Swag Stats

--- trunk/src/main/webapp/resources/js/common.js
+++ trunk/src/main/webapp/resources/js/common.js
@@ -12,3 +12,63 @@ function findElementInForm(form, id) {
 		}
 	}
 }
+
+
+
+
+
+
+
+
+function changeto(table, event, highlightcolor) { 
+	var target=event.target;
+	if (target) {
+		var tagName=target.tagName;
+		if (tagName=="TR" || tagName=="TABLE"){
+			return;
+		}
+		
+		var row = findRow(table, target);
+	
+		highlightRow(row, highlightcolor, true);
+	}
+}
+
+function findRow(table, target) {
+	//  Not working in IE.  ParentNode not an object
+	while(target.parentNode && target.parentNode != table) {
+		target=target.parentNode; 
+		
+		if (target.tagName=="TR") {
+			return target;
+		}
+
+	}
+}
+
+function highlightRow(row, color, saveOriginal) {
+	
+	if (row) {
+		for (i=0; i < row.children.length; i++)
+			var child = row.children[i];
+			if (child.style.backgroundColor!=color) {
+				if (saveOriginal) {
+					child.style.originalColor=child.style.backgroundColor;
+					child.style.backgroundColor=color;
+					
+				} else {
+					child.style.backgroundColor=child.style.originalColor;
+				}
+		
+			}
+		}
+	
+}
+
+function changeback(table, event) {
+	var target=event.target;
+	var row = findRow(table, target);
+	highlightRow(row, '', false);
+}
+
+

54d8896e8c80f43af2485cf21934ca8ac6ab4e73
sambrodkin <sambrodkin@d75cd664-9d54-11de-b988-bd14ec207769>
2009-11-17 14:03:52 +0000

    comments from session attendees

--- src/main/java/com/swagswap/web/springmvc/tags/AbstractSpringContextLookupTag.java
+++ src/main/java/com/swagswap/web/springmvc/tags/AbstractSpringContextLookupTag.java
@@ -10,6 +10,7 @@ import com.swagswap.service.SwagSwapUserService;
 /**
  * Exposes anything from the  spring context for use by subclasses
  * needed since there's no way to do DI in taglibs
+ * TODO this creates another context.  Fix
  *
  */
 public abstract class AbstractSpringContextLookupTag extends TagSupport {
